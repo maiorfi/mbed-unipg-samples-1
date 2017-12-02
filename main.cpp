0b0d17a76edf5bbaeaa2b347439417949d6bcd7c
Lorenzo Maiorfi <maiorfi@innovactive.it>
2017-12-02 15:05:06 +0100

    Gestione lettura stato pulsante con interrupt

--- main.cpp
+++ main.cpp
@@ -2,42 +2,24 @@
 
 Serial debug_serial_port(SERIAL_TX, SERIAL_RX); // uart per i messaggi di debug
 DigitalOut led(LED1);                           // porta digitale di uscita che pilota il led (polarità su scheda: alto->acceso)
-DigitalIn btn(BUTTON1);                         // porta digitale di ingresso che rileva il pulsante blu (polarità su scheda: premuto->basso)
+InterruptIn btnInt(BUTTON1);                    // oggetto per la gestione degli interrupt relativi alla porta digitale di ingresso collegata al pulsante blu (polarità su scheda: premuto->basso)
 
 Thread thread_blink;                            // per il task che fa lampeggiare il led
 Thread thread_recurrent;                        // per il task periodico
-Thread thread_button_poll;                      // per il task che rileva le variazioni di stato del pulsante
 
 EventQueue eq_recurrent;                        // coda che mantiene le operazioni (chiamate a funzioni) del task periodico
-EventQueue eq_button_poll;                      // coda che mantiene le operazioni (chiamate a funzioni) del task che rileva le variazioni di stato del pulsante
 
 Timer timer;                                    // cronometro che misura il tempo trascorso dal boot
 
-int latest_button_state;                        // ultimo stato pulsante rilevato
-
-// task (avviato nel main) che rileva variazioni nello stato corrente del pulsante rispetto al precedente stato rilevato
-void event_function_button_poll()
-{
-    // polarità "inversa" (premuto->basso, ecco perché il "NOT", ossia l'operatore "!")
-    int state = !btn.read();
-
-    // variazione rispetto all'ultimo stato?
-    if (state != latest_button_state)
-    {
-        // messaggio di debug...
-        debug_serial_port.printf("[DEBUG - event_function_button_poll] Stato pulsante : %s\n", state ? "ON" : "OFF");
-
-        // ...e memorizzo quello corrente come ultimo stato registrato
-        latest_button_state = state;
-    }
-}
+unsigned int counterLH;                         // contatore fronti basso->alto
+unsigned int counterHL;                         // contatore fronti alto->basso
 
 // task (avviato nel main) eseguito periodicamente
 // nota: il parametro "c" viene passato dal main(); questo è un modo per avviare più task che eseguono indipendentemente la stessa funzione ma con contesto diverso
 void event_function_recurrent(char c)
 {
     // messaggio di debug; legge il valore del timer/cronometro
-    debug_serial_port.printf("[DEBUG - event_function_recurrent] Trascorsi %d ms (%c)\n", timer.read_ms(), c);
+    debug_serial_port.printf("[DEBUG - event_function_recurrent] Trascorsi %d ms, registrati %d fronti L->H e %d fronti H->L del pulsante blu (%c)\n", timer.read_ms(), counterLH, counterHL, c);
 }
 
 // task (avviato nel main) che in un ciclo infinito effettua il toggling della porta di uscita collegata al led e attende (senza bloccare gli altri task) mezzo secondo
@@ -50,6 +32,11 @@ void thread_function_blink(DigitalOut* pled)
     }
 }
 
+void btn_interrupt_handler()
+{
+    if(btnInt.read()) counterLH++; else counterHL++;
+}
+
 //////////
 // MAIN //
 //////////
@@ -58,14 +45,13 @@ int main()
     debug_serial_port.printf("[DEBUG - main] Lablet RTOS Demo #1 main()...\n");
 
     eq_recurrent.call_every(1000, event_function_recurrent, '*');                       // il task periodico viene "programmato" per essere eseguito una volta al secondo
-    eq_button_poll.call_every(10, event_function_button_poll);                          // il task che rileva lo stato del pusante viene "programmato" per essere eseguito 100 volte al secondo
-
+    
     thread_blink.start(callback(thread_function_blink, &led));                          // il task che effettua il toggling del led viene avviato qui, passando come parametro il puntatore alla porta di uscita da usare
 
+    btnInt.rise(btn_interrupt_handler);                                                 // registrazione handler (funzione di gestione, detta anche isr) L->H
+    btnInt.fall(btn_interrupt_handler);                                                 // registrazione handler (funzione di gestione, detta anche isr) H->L
+    
     timer.start();                                                                      // il cronometro viene avviato qui
     
     thread_recurrent.start(callback(&eq_recurrent, &EventQueue::dispatch_forever));     // il task periodico viene avviato qui
-
-    latest_button_state = !btn.read();                                                  // viene rilevato lo stato iniziale del pulsante; da qui in poi contano solo le variazioni 
-    thread_button_poll.start(callback(&eq_button_poll, &EventQueue::dispatch_forever)); // il task che rileva lo stato del pulsante viene avviato qui
 }
